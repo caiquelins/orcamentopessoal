b2eff2dbb49843d874d62a544ed5e42be38433db
Caique Lins <caique.c.lins@gmail.com>
2021-04-02 18:54:50 -0300

    Criação da Classe BD e Índices Dinâmicos

--- app.js
+++ app.js
@@ -10,6 +10,48 @@ class Despesa {
 	}
 }
 
+class Bd {
+
+	constructor() {
+		//verificar se existe um id em localStorage, caso não exista, iremos setar
+		let id = localStorage.getItem('id');
+
+		if (id === null) {
+			localStorage.setItem('id', 0);
+		}
+	}
+
+	//checar se existe um id inserido em localstorage
+	getProximoId() {
+		//localStorage.getItem serve para recuperar um dado dentro de localStorage
+		let proximoId = localStorage.getItem('id');
+		return parseInt(proximoId) + 1;//converte pra inteiro e soma +1
+	}
+
+	//utilizada para gravar os dados vindos de despesa, dentro do Local Storage do navegador
+	gravar(d) {
+		//pega o id vindo de localStorage
+		let id = localStorage.getItem('id')
+
+		//condicional para verifical se o id é null ou não
+		if(id === null) {
+			id = 0;
+		} else {
+			//pega o próximo id
+			id = this.getProximoId();
+		}
+
+		//acessar Local Storage || JSON.stringify() faz a conversão dos atributos para JSON
+		localStorage.setItem(id, JSON.stringify(d));
+
+		//atualizar o valor contido dentro da chave id, com a informação do novo id, produzido por getProximoId
+		localStorage.setItem('id', id);
+	}
+}
+
+//instanciação da classe Bd
+let bd = new Bd();
+
 function cadastrarDespesa() {
 
 	//variáveis declaradas para referenciar IDs, vindos do HTML
@@ -31,12 +73,5 @@ function cadastrarDespesa() {
 	);
 
 	//chama a função gravar, passando os dados da instância da classe Despesa
-	gravar(despesa);
+	bd.gravar(despesa);
 }
-
-//utilizada para gravar os dados vindos de despesa, dentro do Local Storage do navegador
-function gravar(d) {
-	//acessar Local Storage || JSON.stringify() faz a conversão dos atributos para JSON
-	localStorage.setItem('despesa', JSON.stringify(d));
-}
-
