7433c6944863b27bee333be2707abf2a1e94c587
Caique Lins <caique.c.lins@gmail.com>
2021-04-02 17:29:36 -0300

    Cadastro de Despesa - Parte 1

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("teste")
+function cadastrarDespesa() {
+	
+	//variáveis declaradas para referenciar IDs, vindos do HTML
+	let ano = document.getElementById('ano');
+	let mes = document.getElementById('mes');
+	let dia = document.getElementById('dia');
+	let tipo = document.getElementById('tipo');
+	let descricao = document.getElementById('descricao');
+	let valor = document.getElementById('valor');
+}
